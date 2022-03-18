2ce5335b54ea3196e65c92ed1b65101cefaf4ec7
Mario Cao <mariocao@gmail.com>
2022-03-18 10:40:00 +0100

    refactor(fetch-job): Replace WASM VM with http fetch

--- src/jobs/fetch/FetchJob.ts
+++ src/jobs/fetch/FetchJob.ts
@@ -1,46 +1,22 @@
-import loadBasicFetchBinary from "./loadBinary";
 import logger from "../../services/LoggerService";
 import { DataRequest } from "../../models/DataRequest";
-import { ExecuteResult } from '@fluxprotocol/oracle-vm/dist/models/ExecuteResult';
 import { Job } from "../../models/Job";
 import { Outcome, OutcomeType } from "../../models/Outcome";
 import { createSafeAppConfigString } from '../../models/AppConfig';
-import { execute, InMemoryCache } from '@fluxprotocol/oracle-vm';
 import { executeFetch } from './executeFetch';
 
 export class FetchJob extends Job {
     static type = 'FetchJob';
     id = FetchJob.type;
     type = FetchJob.type;
-    vmCache: InMemoryCache = new InMemoryCache();
 
     async init(): Promise<boolean> {
-        try {
-            await loadBasicFetchBinary();
-            return true;
-        } catch (error) {
-            logger.error(`[${this.id}] ${error}`);
-            return false;
-        }
+        return true;
     }
 
     async executeRequest(request: DataRequest): Promise<Outcome> {
         try {
-            const binary = await loadBasicFetchBinary();
-            const params = {
-                args: request.args,
-                binary,
-                env: {},
-                gasLimit: (300_000_000_000_000).toString(),
-                randomSeed: '0x0001',
-                timestamp: 1,
-            };
-            const executeResult = await execute(params, this.vmCache);
-            console.log("===== VM output ===== \n", executeResult);
-
-            const executeResult2: ExecuteResult = await executeFetch(params);
-            console.log("===== JS output ===== \n", executeResult2);
-
+            const executeResult = await executeFetch(request.args);
             logger.debug(`[${this.id}-${request.internalId}] exit with ${executeResult.code} \n ${executeResult.logs.join('\n')}`);
 
             if (executeResult.code !== 0) {
