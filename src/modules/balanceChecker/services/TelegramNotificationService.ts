c2b17ce529df237e3c223511305d95ee930d837c
Franklin Waller <fr.l.waller@gmail.com>
2022-04-05 16:11:39 +0200

    feat(balancechecker): Add support for telegram stats and error messages

---
+++ src/modules/balanceChecker/services/TelegramNotificationService.ts
@@ -0,0 +1,67 @@
+import { TELEGRAM_BOT_API, TELEGRAM_ALERTS_CHAT_ID, TELEGRAM_STATS_CHAT_ID } from "../../../config";
+import { Network } from "../../../models/Network";
+import logger from "../../../services/LoggerService";
+import { prettySeconds } from "../../../services/TimerUtils";
+import { BalanceReport } from "../models/BalanceReport";
+
+
+export async function notifyTelegramOfBalanceReports(network: Network, reports: BalanceReport[]) {
+    // Stats message (only sent there are alerts of if `TELEGRAM_STATS_CHAT_ID` is set)
+    const balanceErrors = reports.filter(report => report.error);
+    const hasBalanceError = balanceErrors.length > 0;
+    const messages: string[] = [];
+
+    if (hasBalanceError || TELEGRAM_STATS_CHAT_ID) {
+        let message = `${!hasBalanceError ? '✅' : '🆘'} Account Balance update [${network.id}] 💰\n\n`;
+
+        reports.forEach((report) => {
+            message += `\t${!report.error ? '✓' : '⨯'} ${report.address} - ${report.balance.toString()} / ${report.threshold.toString()} \n`;
+        });
+
+        messages.push(message);
+    }
+
+    // Alert message (including addressges of not updated pairs)
+    if (hasBalanceError) {
+        let message = `🆘 [${network.id}] Balances checks have errors 💰 \n\n`;
+
+        balanceErrors.forEach((report) => {
+            message += `\t[${report.address}] - ${report.error}\n`;
+        });
+
+        messages.push(message);
+    }
+
+    // Send to Statistics telegram group
+    if (TELEGRAM_STATS_CHAT_ID) {
+        for await (const message of messages) {
+            await sendTelegramMessage(TELEGRAM_BOT_API, TELEGRAM_STATS_CHAT_ID, message, !hasBalanceError);
+        }
+    }
+
+    // Send to Alerts telegram group
+    if (hasBalanceError && TELEGRAM_ALERTS_CHAT_ID) {
+        for await (const message of messages) {
+            await sendTelegramMessage(TELEGRAM_BOT_API, TELEGRAM_ALERTS_CHAT_ID, message, false);
+        }
+    }
+}
+
+// TODO: Check if response is not 200 OK
+export async function sendTelegramMessage(url: string, chatId: string, text: string, disable_notification?: boolean) {
+    try {
+         await fetch(`${url}/sendMessage`, {
+            method: "POST",
+            body: JSON.stringify({
+                chat_id: chatId,
+                text,
+                disable_notification
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+    } catch (error) {
+        logger.error(`${error}`);
+    }
+}
--- src/services/TimerUtils.ts
+++ src/services/TimerUtils.ts
@@ -16,3 +16,22 @@ export function debouncedInterval(callback: () => Promise<any>, interval: number
         processing = false;
     }, interval);
 }
+
+export function prettySeconds(seconds: number, short?: boolean): string {
+    // Seconds
+    if (seconds < 60) {
+        return Math.floor(seconds) + `${short ? "s" : " seconds"}`;
+    }
+    // Minutes
+    else if (seconds < 3600) {
+        return Math.floor(seconds / 60) + `${short ? "m" : " minutes"}`;
+    }
+    // Hours
+    else if (seconds < 86400) {
+        return Math.floor(seconds / 3600) + `${short ? "h" : " hours"}`;
+    }
+    // Days
+    else {
+        return Math.floor(seconds / 86400) + `${short ? "d" : " days"}`;
+    }
+}
